f49fd029cb0d1b815fb6ec255f96b4af1a4245c0
linamu <linas.murauskas@gmail.com>
2021-10-24 22:49:40 +0300

    Define player turns

--- app/bowling_game.rb
+++ app/bowling_game.rb
@@ -1,23 +1,59 @@
-puts 'Enter Player 1 name'
+class BowlingGame
+  def initialize
+    @player1_score = 0
+    @player2_score = 0
+    @frame = 1
+  end
 
-loop do
-  @player1 = gets.chomp
-  if @player1.empty?
-    puts 'Please enter name'
-  else
-    break
+  def start_game
+    puts 'Enter Player 1 name'
+
+    loop do
+      @player1 = gets.chomp
+      if @player1.empty?
+        puts 'Please enter name'
+      else
+        break
+      end
+    end
+
+    puts 'Enter Player 2 name'
+
+    loop do
+      @player2 = gets.chomp
+      if @player2.empty?
+        puts 'Please enter name'
+      else
+        break
+      end
+    end
+
+    puts 'START OF BOWLING GAME'
+    player1_turn
+  end
+
+  def player1_turn
+    puts "#{@player1_score} taskai"
+    puts "#{@player1} first turn:"
+    player1_points_1 = gets.chomp.to_i
+    @player1_score += player1_points_1
+    return player2_turn if player1_points_1 == 10
+    puts "#{@player1} second turn:"
+    player1_points_2 = gets.chomp.to_i
+    @player1_score += player1_points_2
+    @frame += 0.5
+    player2_turn
+  end
+
+  def player2_turn
+    puts "#{@player2} first turn:"
+    player2_points_1 = gets.chomp.to_i
+    @player2_score += player2_points_1
+    return player1_turn if player2_points_1 == 10
+    puts "#{@player2} second turn:"
+    player2_points_2 = gets.chomp.to_i
+    @player2_score += player2_points_2
+    @frame += 0.5
+    player2_turn
   end
 end
-
-puts 'Enter Player 2 name'
-
-loop do
-  @player2 = gets.chomp
-  if @player2.empty?
-    puts 'Please enter name'
-  else
-    break
-  end
-end
-
-puts 'START OF BOWLING GAME'
---
+++ app/start_game.rb
@@ -0,0 +1,4 @@
+require_relative 'bowling_game'
+
+bowling_game = BowlingGame.new
+bowling_game.start_game
