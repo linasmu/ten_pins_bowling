4866c0f67a83f6ca3c7d664fea81cd02e711ddb4
linamu <linas.murauskas@gmail.com>
2021-10-25 22:16:32 +0300

    Push players frame scores to array

--- app/bowling_game.rb
+++ app/bowling_game.rb
@@ -1,7 +1,7 @@
 class BowlingGame
   def initialize
-    @player1_score = 0
-    @player2_score = 0
+    @player1_score = []
+    @player2_score = []
     @frame = 0
   end
 
@@ -33,28 +33,27 @@ class BowlingGame
   end
 
   def player1_turn
-    return game_result if @frame >= 10
+    return game_result if @frame >= 9
 
+    frame_score = []
     puts "#{@player1} first turn:"
-    player1_points_1 = gets.chomp.to_i
-    @player1_score += player1_points_1
-    return player2_turn if player1_points_1 == 10
+    frame_score << gets.chomp.to_i
+    @player1_score << frame_score and return player2_turn if frame_score[0] == 10
     puts "#{@player1} second turn:"
-    player1_points_2 = gets.chomp.to_i
-    @player1_score += player1_points_2
-    @frame += 0.5
+    frame_score << gets.chomp.to_i
+    @player1_score << frame_score
     player2_turn
   end
 
   def player2_turn
+    frame_score = []
     puts "#{@player2} first turn:"
-    player2_points_1 = gets.chomp.to_i
-    @player2_score += player2_points_1
-    return player1_turn if player2_points_1 == 10
+    frame_score << gets.chomp.to_i
+    @player2_score << frame_score and return player1_turn if frame_score[0] == 10
     puts "#{@player2} second turn:"
-    player2_points_2 = gets.chomp.to_i
-    @player2_score += player2_points_2
-    @frame += 0.5
+    frame_score << gets.chomp.to_i
+    @player2_score << frame_score
+    @frame += 1
     player1_turn
   end
 
